a48782ae91e240c14f9c926b10aecec396870b35
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 14:36:09 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,21 +5,22 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   sorted_arr = arr.sort do
     |a, b|
+    -1
     # binding.pry
-    for i in (0..[a.length, b.length].min - 1) do
-      compare = ESPERANTO_ALPHABET.index(b[i]) <=> ESPERANTO_ALPHABET.index(a[i])
-      binding.pry
-      case compare
-      when -1
-        -1
-        break
-      when 1
-        1
-        break
-      else
-        if i == [a.length, b.length].min - 1 then b.length <=> a.length end
-      end # case
-    end # for
+    # for i in (0..[a.length, b.length].min - 1) do
+    #   compare = ESPERANTO_ALPHABET.index(b[i]) <=> ESPERANTO_ALPHABET.index(a[i])
+    #   binding.pry
+    #   case compare
+    #   when -1
+    #     -1
+    #     break
+    #   when 1
+    #     1
+    #     break
+    #   else
+    #     if i == [a.length, b.length].min - 1 then b.length <=> a.length end
+    #   end # case
+    # end # for
   end # sort
   sorted_arr
 end # alphabetize
