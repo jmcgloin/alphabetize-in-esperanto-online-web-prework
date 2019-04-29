1d4104c33722562d2b63d3ebd58bfaa40f64c135
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 14:17:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,12 +3,17 @@ def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort do
     |a, b|
-    for i in (0..[a.length, b.length].min) do
-      case ESPERANTO_ALPHABET.index(a[i]) <=> ESPERANTO_ALPHABET.index(b[i])
+    for i in (0..[a.length, b.length].min - 1) do
+      case  ESPERANTO_ALPHABET.index(b[i]) <=> ESPERANTO_ALPHABET.index(a[i])
       when -1
         -1
-      when -1
+        break
+      when 1
         1
+        break
+      else
+        if i == [a.length, b.length].min - 1 then 
+        
     end
   # binding.pry
 end
