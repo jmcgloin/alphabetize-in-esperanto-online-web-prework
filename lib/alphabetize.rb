ee7fc898392ddc73da2db3c58a169f5cea638813
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 14:28:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,6 +5,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort do
     |a, b|
+    binding.pry
     for i in (0..[a.length, b.length].min - 1) do
       case  ESPERANTO_ALPHABET.index(b[i]) <=> ESPERANTO_ALPHABET.index(a[i])
       when -1
